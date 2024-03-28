3d5e025f9d3689a6f6fffda44bd2d4fdb00e7932
luminoxmiira gmail <1cda1201>
2024-03-28 23:23:44 +0900

    add localtime

--- createconf.sh
+++ createconf.sh
@@ -14,22 +14,27 @@
 # listen=YES
 # listen_ipv6=NO
 # ------------------
+read -p "Which can it use IPv4 or IPv6?(4/6): " mode
 
 # Allow anonymous FTP?
 # ---------------------
 # anonymous_enable=NO
 # ---------------------
+read -p "Allow anonymous access.(y/n): " ano_en
 
 # Allow to access local users?
 # NOTE: allow to connect local users by written "/etc/passwd"
 # -------------------
 # local_enable=YES
 # -------------------
+read -p "Allow use local users?.(y/n): " use_lo 
 
 # PAM service name (should not to change service name)
 # -------------------------
 # pam_service_name=vsftpd
 # -------------------------
+# This isn't recommended to change name.
+# read -p "What do you use PAM-service name?" service
 
 # login mode(Access restrictions)
 # enable options    YES: enable       NO: disable
@@ -38,27 +43,35 @@
 # userlist_enable=YES
 # userlist_deny=NO
 # -----------------------
+read -p "Do you use list restrictions?(y/n): " list_en
+if [ "$list_en" = "y" ]; then
+    read -p "Do you use whitelists or blacklists?(w/b): " list_mode
+fi
 
 # Use chroot() jail
 # ------------------------------
 # chroot_list_enable=YES
 # allow_writeable_chroot=YES
 # ------------------------------
+read -p "Use chroot()?(y/n): " chroot_en
 
 # chroot list addr
 # -------------------------------------------
 # chroot_list_file=/etc/vsftpd/chroot_list
 # -------------------------------------------
+# File addr are fixed by docker.
 
 # created dir and file umask
 # -----------------
 # local_umask=022
 # -----------------
+read -p "What permission do you use?(default:022): " umask
 
 # use local time
 # --------------------
 # use_localtime=YES
 # --------------------
+read -p "Do you use localtime stamp?(y/n) :" local_time
 
 # use FTP command?
 # ------------------
